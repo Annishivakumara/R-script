34a36e67f318e67a52a052df9e4e42b9c7282325
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-10-15 21:57:25 +0530

    Create problem's.r

---
+++ problem's.r
@@ -0,0 +1,15 @@
+#1.
+data_frame<-data.frame(
+"Training"=c("Strength", "Stamina", "Other"),
+"Pulse"=c(100,200,300),
+"Duration"=c(60,50,40))
+
+print(data_frame)
+
+#2.
+vec1=c(1,2,3,4)
+vec2=c("R", "Scilab","Java")
+vec3=c("For Prototyping","For Prototyping", "For Scailab")
+data_frame=data.frame(vec1,vec2,vec3)
+print(data_frame)
+
